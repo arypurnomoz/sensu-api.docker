2a5c1309aa20a12191e5c26121b713c50ce896ab
Ary Purnomoz <arypurnomoz@gmail.com>
2015-01-15 17:36:55 +0800

    add rabbitmqc config

--- run.sh
+++ run.sh
@@ -7,16 +7,27 @@ fi
 
 cat << EOF > /etc/sensu/config.json
 {
-  "redis": {
-    "host": "$REDIS_HOST",
-    "port": $REDIS_PORT
-  }, 
   "api": {
     "port": 4567,
     "user": "$API_USER",
     "password": "$API_PASS",
     "host": "0.0.0.0",
     "bind": "0.0.0.0"
+  },
+  "redis": {
+    "host": "$REDIS_HOST",
+    "port": $REDIS_PORT
+  }, 
+  "rabbitmq": {
+    "ssl": {
+      "cert_chain_file": "/ssl/cert.pem",
+      "private_key_file": "/ssl/key.pem"
+    },
+    "host": "$RABBITMQ_HOST",
+    "port": $RABBITMQ_PORT,
+    "vhost": "$RABBITMQ_VHOST",
+    "user": "$RABBITMQ_USER",
+    "password": "$RABBITMQ_PASS"
   }
 }
 EOF
